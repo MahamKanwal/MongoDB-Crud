dd104ff52c924598b9ef87800de28f03a70ff0e2
Maham Kanwal <kanwalmahsm@gmail.com>
2025-03-07 08:25:13 +0500

    make a folder

---
+++ app.mjs
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import userRoutes from "./routes/userRoutes.js";
+import connectToDb from "./db/db.js";
+
+app.use(cors());
+app.use(express.json());
+
+// connect to db
+connectToDb()
+
+// middlewares
+
+// app.get("/",(req,res)=>{
+//     res.send("server is active")
+// })
+
+app.use("/api",userRoutes);
+
+app.listen(5000,()=>{
+    console,log("server is listening 5000");
+});
